8b168f354d915cd40447ca627d779c59959c9b89
alexandervansteel <alexandervansteel@gmail.com>
2017-02-07 10:40:42 -0500

    clean up alternate files

--- Project1/HaliteStarterPackage/runGame.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if hash python3 2>/dev/null; then
-    ./halite -d "30 30" "python3 MyBot.py" "python3 OverkillBot.py"
-else
-    ./halite -d "30 30" "python MyBot.py" "python OverkillBot.py"
-fi
